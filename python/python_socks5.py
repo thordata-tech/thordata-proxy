835d135faeb872397489f3f50c9d1059fbd44899
thordata-tech <support@thordata.com>
2024-12-26 10:02:30 +0800

    Create python_socks5.py

---
+++ python/python_socks5.py
@@ -0,0 +1,13 @@
+import requests
+
+username = "username"
+password = "password"
+proxy_server = "t.pr.thordata.net"
+
+proxies = {
+    "http": f"socks5h://{username}:{password}@{proxy_server}",
+    "https": f"socks5h://{username}:{password}@{proxy_server}",
+}
+
+response = requests.get("https://ipinfo.thordata.com", proxies=proxies)
+print(response.text)
