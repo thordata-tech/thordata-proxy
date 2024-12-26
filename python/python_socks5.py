a23367a10d49188fa55792a7a49f07ba1611ab00
thordata-tech <support@thordata.com>
2024-12-26 10:15:58 +0800

    Update python_socks5.py

--- python/python_socks5.py
+++ python/python_socks5.py
@@ -2,7 +2,7 @@ import requests
 
 username = "username"
 password = "password"
-proxy_server = "t.pr.thordata.net"
+proxy_server = "t.pr.thordata.net:12233"
 
 proxies = {
     "http": f"socks5h://{username}:{password}@{proxy_server}",
