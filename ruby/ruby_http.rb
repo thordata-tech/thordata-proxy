4aafa9d2f5c82277c6941e834eb7a0f501ecf724
thordata-tech <support@thordata.com>
2024-12-26 14:59:43 +0800

    Create ruby_http.rb

---
+++ ruby/ruby_http.rb
@@ -0,0 +1,18 @@
+require "uri" 
+require 'net/http' 
+
+proxy_host = 't.pr.thordata.net' 
+proxy_port = 12233 
+proxy_user = 'username' 
+proxy_pass = 'password' 
+
+uri = URI.parse('https://myip.thordata.net') 
+proxy = Net::HTTP::Proxy(proxy_host, proxy_port, proxy_user, proxy_pass) 
+
+req = Net::HTTP::Get.new(uri)
+
+result = proxy.start(uri.host, uri.port, use_ssl: true) do |http| 
+  http.request(req) 
+end 
+
+puts result.body
