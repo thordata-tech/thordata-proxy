57045f467391a08ccfb432f5bd07b712bdfd0b2d
thordata-tech <support@thordata.com>
2024-12-26 10:14:14 +0800

    Update python_http.py

--- python/python_http.py
+++ python/python_http.py
@@ -2,7 +2,7 @@ import requests
 
 username = "username"
 password = "password"
-proxy_server = "t.pr.thordata.net"
+proxy_server = "t.pr.thordata.net:12233"
 
 proxies = {"http": f"http://{username}:{password}@{proxy_server}"}
 response = requests.get("https://ipinfo.thordata.com", proxies=proxies)
