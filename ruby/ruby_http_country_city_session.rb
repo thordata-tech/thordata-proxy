86f8a7efcb7b47aa4031f4250613c942e6fa8b3e
thordata-tech <support@thordata.com>
2024-12-30 11:14:38 +0800

    Create ruby_http_country_city_session.rb

---
+++ ruby/ruby_http_country_city_session.rb
@@ -0,0 +1,23 @@
+require "uri" 
+require 'net/http' 
+
+proxy_host = 't.pr.thordata.net' 
+proxy_port = 12233 
+proxy_username = 'username'
+proxy_pass = 'password'
+proxy_country = 'us'
+proxy_city = 'houston'
+proxy_sessid = 'a123123'
+proxy_sesstime = 10
+proxy_user = "user-#{proxy_username}-region-#{proxy_country}-city-#{proxy_city}-sessid=#{proxy_sessid}-sesstime=#{proxy_sesstime}"
+
+uri = URI.parse('https://myip.thordata.net') 
+proxy = Net::HTTP::Proxy(proxy_host, proxy_port, proxy_user, proxy_pass) 
+
+req = Net::HTTP::Get.new(uri)
+
+result = proxy.start(uri.host, uri.port, use_ssl: true) do |http| 
+  http.request(req) 
+end 
+
+puts result.body
