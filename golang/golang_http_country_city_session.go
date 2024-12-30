df66bd80e330938485fc05aa8aebe39904d0923f
thordata-tech <support@thordata.com>
2024-12-30 11:34:41 +0800

    Create golang_http_country_city_session.go

---
+++ golang/golang_http_country_city_session.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+const (
+	resourceUrl = "https://ipinfo.thordata.com"
+	proxyHost   = "hiesllxm.pr.thordata.net:12233"
+	username    = "username"
+	password    = "password"
+	country     = "us"
+	city        = "houston"
+	sessid      = "a123123"
+	sesstime    = 10
+)
+
+func main() {
+	proxyUserName := fmt.Sprintf("user-%s-region-%s-city-%s-sessid-%s-sesstime-%d", username, country, city, sessid, sesstime)
+	proxyUrl := &url.URL{
+		Scheme: "http",
+		User:   url.UserPassword(proxyUserName, password),
+		Host:   proxyHost,
+	}
+
+	client := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+
+	resp, err := client.Get(resourceUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyString := string(body)
+	fmt.Println(bodyString)
+}
