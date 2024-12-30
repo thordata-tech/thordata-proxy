2070cd2744aa77b00d8aa32a4130452ea827a9f2
thordata-tech <support@thordata.com>
2024-12-30 11:24:39 +0800

    Create node_http_country.js

---
+++ node/node_http_country.js
@@ -0,0 +1,17 @@
+const rp = require('request-promise');
+
+const username = "username";
+const password = "password";
+const proxyServer = "t.pr.thordata.net:12233";
+const country = "us";
+
+rp({
+    url: 'http://ipinfo.thordata.com',  
+    proxy: `http://user-${username}-region-${country}:${password}@${proxyServer}`,  
+})
+.then(function(data) {
+    console.log(data);  
+})
+.catch(function(err) {
+    console.error(err); 
+});
