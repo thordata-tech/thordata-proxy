68a806e25b8ba6adb0f06c22839d3405eb35d9a7
thordata-tech <support@thordata.com>
2024-12-30 11:02:39 +0800

    Create ruby_http_country.rb

---
+++ ruby/ruby_http_country.rb
@@ -0,0 +1,20 @@
+require "uri" 
+require 'net/http' 
+
+proxy_host = 't.pr.thordata.net' 
+proxy_port = 12233 
+proxy_username = 'username'
+proxy_pass = 'password'
+proxy_country = 'us'
+proxy_user = "user-#{proxy_username}-region-#{proxy_country}"
+
+uri = URI.parse('https://myip.thordata.net') 
+proxy = Net::HTTP::Proxy(proxy_host, proxy_port, proxy_user, proxy_pass) 
+
+req = Net::HTTP::Get.new(uri)
+
+result = proxy.start(uri.host, uri.port, use_ssl: true) do |http| 
+  http.request(req) 
+end 
+
+puts result.body
