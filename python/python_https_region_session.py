3c38b173b6a304dc0854357cdffdabc5f5cb9361
thordata-tech <support@thordata.com>
2024-12-31 10:44:25 +0800

    Create python_https_region_session.py

---
+++ python/python_https_region_session.py
@@ -0,0 +1,16 @@
+import requests
+
+
+username = "username"
+password = "password"
+proxy_server = "t.pr.thordata.net:12233"
+country = "us"
+city = "houston"
+sessid = "a123123"
+sesstime = 10
+
+proxies = {
+    "http": f"https://user-{username}-region-{country}-city-{city}-sessid-{sessid}-sesstime-{sesstime}:{password}@{proxy_server}"
+}
+response = requests.get("https://ipinfo.thordata.com", proxies=proxies)
+print(response.text)
