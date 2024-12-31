a86cb3e1f4704ca549cf79226f7775455015f429
thordata-tech <support@thordata.com>
2024-12-31 11:47:48 +0800

    Create node_socks5_region.js

---
+++ node/node_socks5_region.js
@@ -0,0 +1,21 @@
+const rp = require('request-promise');
+const { SocksProxyAgent } = require('socks-proxy-agent');  
+
+const username = "username";
+const password = "password";
+const proxyServer = "t.pr.thordata.net:12233";
+const country = "us";
+
+const proxyUrl = `socks5h://user-${username}-region-${country}:${password}@${proxyServer}`;
+const agent = new SocksProxyAgent(proxyUrl);  
+
+rp({
+    url: 'https://ipinfo.thordata.com',  
+    agent: agent,  
+})
+.then(function(data) {
+    console.log(data);  
+})
+.catch(function(err) {
+    console.error(err);  
+});
