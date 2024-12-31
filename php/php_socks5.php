12e2c158338d95e1e4843f3fdfeda6096228d9f0
thordata-tech <support@thordata.com>
2024-12-31 11:36:30 +0800

    Update php_socks5.php

--- php/php_socks5.php
+++ php/php_socks5.php
@@ -1,19 +1,21 @@
-$url = 'myip.thordata.net';
-$proxy = 't.pr.thordata.net';
-$port = 12233;
-$user = 'username';
-$psw = 'password';
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-curl_setopt($ch, CURLOPT_PROXY, "$proxy:$port");
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$user:$psw");
-$result = curl_exec($ch);
-curl_close($ch);
-
-if ($result)
-{
-    echo $result . PHP_EOL;
-}
+<?php
+    $url = 'myip.thordata.net';
+    $proxy = 't.pr.thordata.net';
+    $port = 12233;
+    $user = 'username';
+    $psw = 'password';
+    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    
+    curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+    curl_setopt($ch, CURLOPT_PROXY, "$proxy:$port");
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$user:$psw");
+    $result = curl_exec($ch);
+    curl_close($ch);
+    
+    if ($result)
+    {
+        echo $result . PHP_EOL;
+    }
+?>
