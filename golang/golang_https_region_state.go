387f19c7542595e3e4e9cfbd026c6715ee43dc35
thordata-tech <support@thordata.com>
2024-12-31 13:53:47 +0800

    Create golang_https_region_state.go

---
+++ golang/golang_https_region_state.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+const (
+	resourceUrl = "https://ipinfo.thordata.com"
+	proxyHost   = "t.pr.thordata.net:12233"
+	username    = "username"
+	password    = "password"
+	country     = "us"
+	state       = "alabama"
+)
+
+func main() {
+	proxyUserName := fmt.Sprintf("user-%s-region-%s-st-%s", username, country, state)
+	proxyUrl := &url.URL{
+		Scheme: "https",
+		User:   url.UserPassword(proxyUserName, password),
+		Host:   proxyHost,
+	}
+
+	client := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+
+	resp, err := client.Get(resourceUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyString := string(body)
+	fmt.Println(bodyString)
+}
