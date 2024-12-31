061cf8fde989a0a3f54febe62a08f05075b1af71
thordata-tech <support@thordata.com>
2024-12-31 10:13:45 +0800

    Update and rename python_http_region_st.py to python_http_region_state.py

--- python/python_http_region_state.py
+++ python/python_http_region_state.py
@@ -5,10 +5,10 @@ username = "username"
 password = "password"
 proxy_server = "t.pr.thordata.net:12233"
 country = "us"
-st = "alabama"
+state = "alabama"
 
 proxies = {
-    "http": f"http://user-{username}-region-{country}-st-{st}:{password}@{proxy_server}"
+    "http": f"http://user-{username}-region-{country}-st-{state}:{password}@{proxy_server}"
 }
 response = requests.get("http://ipinfo.thordata.com", proxies=proxies)
 print(response.text)
