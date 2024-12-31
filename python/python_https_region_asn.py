8a98490988c20fddc0f073f30a9f518a632c1cc9
thordata-tech <support@thordata.com>
2024-12-31 10:42:21 +0800

    Create python_https_region_asn.py

---
+++ python/python_https_region_asn.py
@@ -0,0 +1,16 @@
+import requests
+
+
+username = "username"
+password = "password"
+proxy_server = "t.pr.thordata.net:12233"
+country = "us"
+asn = "AS10796"
+
+proxies = {
+    "http": f"https://user-{username}-region-{country}-asn-{asn}:{password}@{proxy_server}"
+}
+
+response = requests.get("https://ipinfo.thordata.com", proxies=proxies)
+
+print(response.text)
