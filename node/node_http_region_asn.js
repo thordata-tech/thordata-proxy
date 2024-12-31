095315767489896605bdeaf2bcdbb0c99a6ea675
thordata-tech <support@thordata.com>
2024-12-31 11:40:36 +0800

    Create node_http_region_asn.js

---
+++ node/node_http_region_asn.js
@@ -0,0 +1,18 @@
+const rp = require('request-promise');
+
+const username = "username";
+const password = "password";
+const proxyServer = "t.pr.thordata.net:12233";
+const country = "us";
+const asn = "AS10796";
+
+rp({
+    url: 'http://ipinfo.thordata.com',  
+    proxy: `http://user-${username}-region-${country}-asn-${asn}:${password}@${proxyServer}`,  
+})
+.then(function(data) {
+    console.log(data);  
+})
+.catch(function(err) {
+    console.error(err); 
+});
