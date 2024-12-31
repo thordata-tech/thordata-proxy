aba2d240b59536d2564697f7e89f54622302260b
thordata-tech <support@thordata.com>
2024-12-31 11:45:11 +0800

    Create node_https_region_city.js

---
+++ node/node_https_region_city.js
@@ -0,0 +1,18 @@
+const rp = require('request-promise');
+
+const username = "username";
+const password = "password";
+const proxyServer = "t.pr.thordata.net:12233";
+const country = "us";
+const city = "houston";
+
+rp({
+    url: 'http://ipinfo.thordata.com',  
+    proxy: `https://user-${username}-region-${country}-city-${city}:${password}@${proxyServer}`,  
+})
+.then(function(data) {
+    console.log(data);  
+})
+.catch(function(err) {
+    console.error(err); 
+});
