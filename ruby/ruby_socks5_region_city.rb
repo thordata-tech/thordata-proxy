d7d1f1a1a3badfec5947e29f2f61602a89cc6463
thordata-tech <support@thordata.com>
2024-12-31 11:27:58 +0800

    Create ruby_socks5_region_city.rb

---
+++ ruby/ruby_socks5_region_city.rb
@@ -0,0 +1,23 @@
+require 'socksify'
+require 'net/http'
+
+proxy_host = 't.pr.thordata.net' 
+proxy_port = 12233 
+proxy_username = 'username'
+proxy_pass = 'password'
+proxy_country = 'us'
+proxy_city = 'houston'
+proxy_user = "user-#{proxy_username}-region-#{proxy_country}-city-#{proxy_city}"
+
+TCPSocket::socks_server = proxy_host
+TCPSocket::socks_port = proxy_port
+TCPSocket::socks_username = proxy_user
+TCPSocket::socks_password = proxy_pass
+
+uri = URI('http://myip.thordata.net')
+http = Net::HTTP.new(uri.host, uri.port)
+
+request = Net::HTTP::Get.new(uri)
+
+response = http.request(request)
+puts response.body
