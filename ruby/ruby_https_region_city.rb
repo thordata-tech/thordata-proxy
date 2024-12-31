15db575257f7622dec98807405f9d34deaa18bcc
thordata-tech <support@thordata.com>
2024-12-31 11:23:10 +0800

    Create ruby_https_region_city.rb

---
+++ ruby/ruby_https_region_city.rb
@@ -0,0 +1,23 @@
+require "uri"
+require 'net/http'
+require 'openssl'
+
+proxy_host = 't.pr.thordata.net' 
+proxy_port = 12233 
+proxy_username = 'username'
+proxy_pass = 'password'
+proxy_country = 'us'
+proxy_city = 'houston'
+proxy_user = "user-#{proxy_username}-region-#{proxy_country}-city-#{proxy_city}"
+
+uri = URI.parse('https://myip.thordata.net')
+
+proxy = Net::HTTP::Proxy(proxy_host, proxy_port, proxy_user, proxy_pass)
+
+req = Net::HTTP::Get.new(uri)
+
+result = proxy.start(uri.host, uri.port, use_ssl: true, ssl_version: :TLSv1_2) do |http|
+  http.request(req)
+end
+
+puts result.body
