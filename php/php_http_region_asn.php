3a9e74b4bb79d3354b55b3e875ac428be1d39783
thordata-tech <support@thordata.com>
2024-12-31 11:30:31 +0800

    Create php_http_region_asn.php

---
+++ php/php_http_region_asn.php
@@ -0,0 +1,24 @@
+<?php
+  $url = 'myip.thordata.net';
+  $proxy = 't.pr.thordata.net';
+  $port = 12233;
+  $user = 'username';
+  $psw = 'password';
+  $country = 'us';
+  $asn = 'AS10796';
+  $proxyuser = sprintf("user-%s-region-%s-asn-%s",$user,$country,$asn);
+
+
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  
+  curl_setopt($ch, CURLOPT_PROXY, "$proxy:$port");
+  curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$proxyuser:$psw");
+  $result = curl_exec($ch);
+  curl_close($ch);
+  
+  if ($result)
+  {
+      echo $result . PHP_EOL;
+  }
+?>
