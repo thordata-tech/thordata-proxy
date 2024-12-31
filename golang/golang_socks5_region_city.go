84f602eea7860edb557f6de26fb239592216ec93
thordata-tech <support@thordata.com>
2024-12-31 14:02:00 +0800

    Create golang_socks5_region_city.go

---
+++ golang/golang_socks5_region_city.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/proxy"
+)
+
+const (
+	resourceUrl = "https://ipinfo.thordata.com"
+	socks5Proxy = "t.pr.thordata.net:12233"
+	username    = "username"
+	password    = "password"
+	country     = "us"
+	city        = "houston"
+)
+
+func main() {
+	proxyUserName := fmt.Sprintf("user-%s-region-%s-city-%s", username, country, city)
+	proxyUrl := socks5Proxy
+
+	auth := proxy.Auth{
+		User:     proxyUserName,
+		Password: password,
+	}
+
+	dialer, err := proxy.SOCKS5("tcp", proxyUrl, &auth, proxy.Direct)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := http.Client{
+		Transport: &http.Transport{
+			Dial: dialer.Dial,
+		},
+	}
+
+	resp, err := client.Get(resourceUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyString := string(body)
+	fmt.Println(bodyString)
+}
